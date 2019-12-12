44abc458dc06dfb4fc863682c0e6ac6bb1d3db57
Huy Phan <hello@example.com>
2019-12-13 00:52:11 +0700

    Vietsub phần dành cho customer

--- Website_BanDienThoai_Version1/Areas/Admin/Controllers/StaticticalController.cs
+++ Website_BanDienThoai_Version1/Areas/Admin/Controllers/StaticticalController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Website_BanDienThoai_Version1.Data;
+using Website_BanDienThoai_Version1.Models;
 
 namespace Website_BanDienThoai_Version1.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class StaticticalController : Controller
     {
-        [Area("Admin")]
+      
+
+        private readonly ApplicationDbContext _db;
+       
+        public StaticticalController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(_db.Contact.OrderByDescending(f=>f.ContactDate).ToList());
         }
     }
 }
